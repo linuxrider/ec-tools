7138c37d1ceb26e52514cc90679d6d673fa5b9b5
Johannes Hermann <linuxrider@web.de>
2022-05-09 11:30:58 +0200

    add determine_scan_rate

--- ec_tools/helper.py
+++ ec_tools/helper.py
@@ -26,4 +26,16 @@ def find_x0_values(x, y, mode="all"):
 
     m = (y[crossings] - y[crossings+1]) / (x[crossings] - x[crossings+1])
     Δx = -y[crossings] / m 
-    return Δx+x[crossings]
+    return Δx + x[crossings]
+
+def determine_scan_rate(t, x):
+    """Return scan rate of given t and x arrays.
+
+    TEST:
+    >>> t = np.array([0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32])
+    >>> E = np.array([0.1, 0.11, 0.12, 0.13, 0.14, 0.15, 0.14, 0.13, 0.12, 0.11, 0.10, 0.09, 0.10, 0.11, 0.12, 0.13, 0.14])
+    >>> determine_scan_rate(t, E)
+    0.005
+    """
+
+    return np.abs(np.diff(x) / np.diff(t)).mean()
